afcd528e577417de75c9ec923253fc435ab8f1da
Stefan Wolff <wolffste@gmail.com>
2020-02-18 19:20:59 +0100

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.word,
+    content: Faker::Lorem.paragraph(sentence_count: 2)
+    )
+  article.save!
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
